6f4f497e3ae7f6f11a1b1c6b2a5a41635f6549ad
breckiam <breckiam@gmail.com>
2021-12-10 19:42:18 -0700

    Multi var printing

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,15 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("State: " + state);
+
+   System.out.print("\nHello " + name + "! I see you're " + age);
+    System.out.print(" and from " + state);
+  }
+
+}
