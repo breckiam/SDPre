3367cc89abe8bea17038f6795ef073b050832ac4
breckiam <breckiam@gmail.com>
2021-12-10 23:21:01 -0700

    Objective2Lab3.java

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,9 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1 =21, num2 = 14, num3 = 57, average;
+
+    average = (num1 + num2 + num3) / 3;
+
+    System.out.println("The average of the three numbers: " + average);
+  }
+}
