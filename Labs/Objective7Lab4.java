78936bbc0eede31217a3bbea8f1c29c9b6025190
breckiam <breckiam@gmail.com>
2021-12-16 20:00:26 -0700

    Sum numbers

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,13 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int sum = 0, counter = 1;
+
+    while (counter <= 20) {
+      sum += counter;
+      counter++;
+    }
+
+    System.out.println(sum);
+  }
+
+}
