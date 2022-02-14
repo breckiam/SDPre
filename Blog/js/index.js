e930dcff9dcec7a9d0bcadb36a35be9709876d4a
breckiam <breckiam@gmail.com>
2022-02-14 08:10:27 -0700

    V2.0 Blog / full redesign

--- Blog/js/index.js
+++ Blog/js/index.js
@@ -1,41 +1,23 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelector('.nav-link');
-let img = document.getElementById('mac');
-let btn1 = document.getElementById('btn1');
-let btn2 = document.getElementById('btn2');
-let btn3 = document.getElementById('btn3');
-let description = document.getElementById('2');
-let title = document.getElementById('1');
-let link = document.getElementById('3');
+const toggleSwitch = document.querySelector('#checkbox');
+const currentTheme = localStorage.getItem('theme');
 
-btn1.addEventListener('click', () => {
-    img.src= 'img/mac-elevated.png';
-    description.innerHTML = 'Created using Wordpress with the use of elementor to adjust layout and styling.';
-    title.innerHTML = 'Elevated Take-off';
-    link.href = 'https://elevatedtakeoff.com/'
 
-})
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
 
-btn2.addEventListener('click', () => {
-    img.src= 'img/mac-epik.png';
-    description.innerHTML = 'Created using Wordpress with the use of elementor to adjust layout and styling.';
-    title.innerHTML = 'Epik Media';
-    link.href = 'https://epikmedia.co/'
-})
+    if (currentTheme === 'light') {
+        toggleSwitch.checked = true;
+    }
+}
 
-btn3.addEventListener('click', () => {
-    img.src= 'img/mac-ies.png';
-    description.innerHTML = 'Created using HTML, CSS, and JavaScript. Websited started from a template and was modified to current look and functionality.';
-    title.innerHTML = 'IES CORP.';
-    link.href = 'https://iescorporation.net/'
-})
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+    else {        document.documentElement.setAttribute('data-theme', 'dark');
+          localStorage.setItem('theme', 'dark');
+    }
+}
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-})
-
-navLinks.forEach(link => {
-    link.addEventListener('click', ()  => {
-      document.body.classList.remove('nav-open');
-    })
-})
+toggleSwitch.addEventListener('change', switchTheme, false);
